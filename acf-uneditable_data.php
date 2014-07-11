dd8e5e425b0efc982ae3c4f8c030721c97e4b319
Björn <b@folbert.com>
2014-07-11 09:17:32 +0200

    First

---
+++ acf-uneditable_data.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Uneditable data
+Plugin URI: https://github.com/firstflight/acf-field-type-uneditable-data
+Field type for Advanced Custom Field to display data and preventing the user from editing it.: Field type for Advanced Custom Field to display data and preventing the user from editing it.
+Version: 1.0.0
+Author: Björn Folbert for First Flight Communication
+Author URI: http://www.firstflight.se
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+
+
+// 1. set text domain
+// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+load_plugin_textdomain( 'acf-uneditable_data', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+
+
+
+
+// 2. Include field type for ACF5
+// $version = 5 and can be ignored until ACF6 exists
+function include_field_types_uneditable_data( $version ) {
+	
+	include_once('acf-uneditable_data-v5.php');
+	
+}
+
+add_action('acf/include_field_types', 'include_field_types_uneditable_data');
+
+
+
+
+// 3. Include field type for ACF4
+function register_fields_uneditable_data() {
+	
+	include_once('acf-uneditable_data-v4.php');
+	
+}
+
+add_action('acf/register_fields', 'register_fields_uneditable_data');
+
+
+
+	
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Advanced Custom Fields: Uneditable data Field ===
+Contributors: Björn Folbert for First Flight Communication
+Tags: acf, acf field, uneditable data
+Requires at least: 3.5
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Field type for Advanced Custom Field to display data and preventing the user from editing it.
+
+== Field type for Advanced Custom Field to display data and preventing the user from editing it. ==
+
+EXTENDED_Field type for Advanced Custom Field to display data and preventing the user from editing it.
+
+= Compatibility =
+
+This ACF field type is compatible with:
+* ACF 5
+* ACF 4
+
+== Installation ==
+
+1. Copy the `acf-uneditable_data` folder into your `wp-content/plugins` folder
+2. Activate the Uneditable data plugin via the plugins admin page
+3. Create a new field via ACF and select the Uneditable data type
+4. Please refer to the Field type for Advanced Custom Field to display data and preventing the user from editing it. for more info regarding the field type settings
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release.
